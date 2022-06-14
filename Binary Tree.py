e5222a0383d1511a40b274a20ac90ef9ce4f68a2
kirapty <95729436+kirapty@users.noreply.github.com>
2022-06-13 19:47:42 -0500

    Add files via upload

---
+++ Binary Tree.py
@@ -0,0 +1,133 @@
+import os
+
+class Node():
+    def __init__(self, dato):
+        self.left = None
+        self.right = None
+        self.dato = dato
+
+class arbol():
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, a, dato):
+        if a == None:
+            a = Node(dato)
+        else:
+            d = a.dato
+            if dato < d:
+                a.left = self.insert(a.left, dato)
+            else:
+                a.right = self.insert(a.right, dato)
+        return a
+
+    def inorder(self, a):
+        if a == None:
+            return None
+        else:
+            self.inorder(a.left)
+            print(a.dato)
+            self.inorder(a.right)
+
+    def preorder(self, a):
+        if a == None:
+            return None
+        else:
+            print(a.dato)
+            self.preorder(a.left)
+            self.preorder(a.right)
+
+    def postorder(self, a):
+        if a == None:
+            return None
+        else:
+            self.postorder(a.left)
+            self.postorder(a.right)
+            print(a.dato)
+
+    def buscar(self, dato, a):
+        if a == None:
+            return None
+        else:
+            if dato == a.dato:
+                return a.dato
+            else:
+                if dato < a.dato:
+                    return self.buscar(dato, a.left)
+                else:
+                    return self.buscar(dato, a.right)
+
+tree = arbol()
+
+while True:
+    os.system("cls")
+    print("Arbol ABB")
+    opc = input("\n1.-Insertar nodo \n2.-Inorden \n3.-Preorden \n4.-Postorden \n5.-Buscar \n6.-Ver si esta Balanceado \n7.-Salir\nElige una opcion -> ")
+
+    if opc == '1':
+        nodo = input("\nIngresa el nodo -> ")
+        if nodo.isdigit():
+            nodo = int(nodo)
+            tree.root = tree.insert(tree.root, nodo)
+        else:
+            print("\nIngresa solo digitos...")
+    elif opc == '2':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.inorder(tree.root)
+    elif opc == '3':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.preorder(tree.root)
+    elif opc == '4':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.postorder(tree.root)
+    elif opc == '5':
+        nodo = input("\nIngresa el nodo a buscar -> ")
+        if nodo.isdigit():
+            nodo = int(nodo)
+            if tree.buscar(nodo, tree.root) == None:
+                print("\nNodo no encontrado...")
+            else:
+                print("\nNodo encontrado -> ",tree.buscar(nodo, tree.root), " si existe...")
+        else:
+            print("\nIngresa solo digitos...")        
+    elif opc == '6':
+        class Height:
+            def __init__(self):
+                self.height = 0
+        def isHeightBalanced(root, height):
+            left_height = Height()
+            right_height = Height()
+            if root is None:
+                return True
+            l = isHeightBalanced(root.left, left_height)
+            r = isHeightBalanced(root.right, right_height)
+            height.height = max(left_height.height, right_height.height) + 1
+            if abs(left_height.height - right_height.height) <= 1:
+                return l and r
+            return False
+        height = Height()
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        if isHeightBalanced(root, height):
+            print('El arbol esta balanceado')
+        else:
+            print('El arbol no esta balanceado')
+    elif opc == '7':
+        print("\nElegiste salir...\n")
+        os.system("pause")
+        break
+    else:
+        print("\nElige una opcion correcta...")
+    print()
+    os.system("pause")
+
+print()
